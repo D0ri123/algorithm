96675f363defb53d496a2758da0c01f5f476f8ee
D0ri123 <lsh81550@gmail.om>
2024-06-09 15:56:31 +0900

    삼각달팽이

---
+++ programmers/TriangleSnail.java
@@ -0,0 +1,52 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class TriangleSnail {
+    private static final int[] dx = {0, 1, -1};
+    private static final int[] dy = {1, 0, -1};
+
+    public int[] solution(int n) {
+        int[][] triangle = new int[n][n];
+        int v = 1;
+        int x = 0;
+        int y = 0;
+        int d = 0;
+
+        while (true) {
+            triangle[y][x] = v++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            // 진행방향대로 더이상 움직일 수 없을 때
+            if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+                d = (d+1) % 3;
+                nx = x + dx[d];
+                ny = y + dy[d];
+                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+                    break;
+                }
+            }
+
+            x = nx;
+            y = ny;
+        }
+
+        int[] result = new int[v - 1];
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                result[index++] = triangle[i][j];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        TriangleSnail problem = new TriangleSnail();
+
+        System.out.println(Arrays.toString(problem.solution(n)));
+    }
+
+}
